0954ebbc266e399668f57e24e32a9a9db48759b8
D_coding <iopgoom@naver.com>
2022-02-24 18:06:35 +0900

    220224

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,5 @@
+from dataclasses import field
+from statistics import mode
 from django.contrib import admin
 from catalog.models import Author, Book, Genre, BookInstance, Language
 
@@ -10,16 +12,23 @@ admin.site.register(Genre)
 admin.site.register(Language)
 
 
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = (
-        "last_name",
-        "first_name",
-        "date_of_birth",
-        "date_of_death",
-    )
+    list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
+    fields = ["first_name", "last_name", ("date_of_birth", "date_of_death")]
+    inlines = [BookInline]
 
 
 admin.site.register(Author, AuthorAdmin)
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
 
 
 @admin.register(Book)
@@ -30,8 +39,40 @@ class BookAdmin(admin.ModelAdmin):
         "display_genre",
         "language",
     )
+    inlines = [BooksInstanceInline]
 
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "book",
+        "status",
+        "due_back",
+        "id",
+    )
+    list_filter = (
+        "status",
+        "due_back",
+    )
+
+    fieldsets = (
+        (
+            "book infomation",
+            {
+                "fields": (
+                    "book",
+                    "imprint",
+                    "id",
+                )
+            },
+        ),
+        (
+            "Availability",
+            {
+                "fields": (
+                    "status",
+                    "due_back",
+                )
+            },
+        ),
+    )
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from catalog.models import Book, BookInstance, Author, Genre, Language
 
 # Create your views here.
+def index(request):
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+    }
+    return HttpResponse(book_num)
